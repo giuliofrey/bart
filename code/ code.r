2872caf85b26e8805e3160b7fed41fabda7abcc8
Giulio Frey <giulio76frey@gmail.com>
2024-12-02 20:17:57 +0100

    Work on code

--- code/ code.r
+++ code/ code.r
@@ -2,7 +2,8 @@
 packages <- c(
     "ggplot2", 
     "dplyr",
-    "BayesTree", # For Bayesian Additive Regression Trees
+    "MASS",
+    "BART",
     "gbm", # For Generalized Boosted Regression Models
     "randomForest" # For Random Forest
 )
@@ -14,3 +15,34 @@ if (any(installed_packages == FALSE)) {
 
 # Packages loading
 invisible(lapply(packages, library, character.only = TRUE))
+
+# Settting the seed
+set.seed(123)
+
+x <- Boston[, c(6, 13)] 
+
+dim(x)
+
+y <- Boston$medv
+
+nd <- 200 # The number of posterior draws
+burn <- 50 # Number of MCMC iterations to be treated as burn in
+
+
+bart <- wbart(x, y, nskip = burn, ndpost = nd, ntree = 1000)
+bart.mse <- mean((bart$yhat.train.mean - y)^2)
+
+rf <- randomForest(x, y, ntree = 1000)
+
+
+# TODO understand the structure of the gbm object and why works only with data frame
+gbm <- gbm(medv ~ ., data = Boston, n.trees = 1000, distribution = "gaussian", interaction.depth = 1)
+
+#TODO implement bagging
+
+mse_df <- data.frame(
+    mse = c(bart.mse, mean(rf$mse), mean(gbm$train.error)),
+    method = c("BART", "Random Forest", "GBM")
+)
+
+print(mse_df)
