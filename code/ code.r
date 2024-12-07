fc200103fcdc775abc3100b030658a0533eeb5d5
Giulio Frey <giulio76frey@gmail.com>
2024-12-07 10:34:58 +0100

    created code for multiple runs to test optimal number of trees

--- code/ code.r
+++ code/ code.r
@@ -5,7 +5,8 @@ packages <- c(
     "MASS",
     "BART",
     "gbm", # For Generalized Boosted Regression Models
-    "randomForest" # For Random Forest
+    "randomForest", # For Random Forest,
+    "ucimlrepo" # For the Abalone age prediction dataset,
 )
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -19,71 +20,72 @@ invisible(lapply(packages, library, character.only = TRUE, warn.conflicts = FALS
 # Settting the seed
 set.seed(123)
 
-# Load the full Boston dataset and create a dataframe
-train <- sample(1:nrow(Boston), nrow(Boston) / 2)
-boston_df <- as.data.frame(Boston)
-x <- Boston[, 1:12]
-y <- Boston[, "medv"]
+#load the crab dataset
+abalone <- fetch_ucirepo(name = "Abalone")
+
+abalone_df <- abalone$data$original
+
+
+#convert the categorical variables to factors
+abalone_df$Sex <- as.factor(abalone_df$Sex)
+
+#convert rings to age
+abalone_df$Age <- abalone_df$Rings + 1.5
+#remove the rings column
+abalone_df <- abalone_df[, setdiff(names(abalone_df), "Rings")]
+
+
+train <- sample(1:nrow(abalone_df), nrow(abalone_df) / 2)
+x <- abalone_df[,  setdiff(names(abalone_df), "Age")]
+y <- abalone_df[, "Age"]
 xtrain <- x[train, ]
 ytrain <- y[train]
 xtest <- x[-train, ]
 ytest <- y[-train]
 
-# Load an underspecified dataset
-boston_under_df <- as.data.frame(Boston[, c(6, 13, 14)])
-train_under <- sample(1:nrow(boston_under_df), nrow(boston_under_df) / 2)
-x_under <- Boston[, c(6, 13)]
-y_under <- Boston[, "medv"]
-xtrain_under <- x_under[train_under, ]
-ytrain_under <- y_under[train_under]
-xtest_under <- x_under[-train_under, ]
-ytest_under <- y_under[-train_under]
 
-nd <- 200 # The number of posterior draws
-burn <- 50 # Number of MCMC iterations to be treated as burn in
-
-#Bayesian Additive Regression Trees
-bart <- gbart(xtrain, ytrain, x.test = xtest)
-bart.mse <- mean((bart$yhat.test.mean - y)^2)
-
-#underspecified model
-bart_under <- wbart(xtrain_under, ytrain_under, x.test = xtest_under)
-bart_under.mse <- mean((bart_under$yhat.test.mean - y)^2)
-
-# Random Forest
-rf <- randomForest(medv ~ ., data = boston_df, ntree = 1000, subset = train)
-rf.mse >- mean((rf$predicted - y)^2)
-
-# underspecified model
-rf_under <- randomForest(medv ~ ., data = boston_under_df, ntree = 1000, subset = train)
-
-# TODO: Calculate the MSE
-# Generalized Boosted Regression Models
-gbm <- gbm(medv ~ ., data = Boston[train, ], n.trees = 1000, distribution = "gaussian", interaction.depth = 1)
-gbm.yhat <- predict(gbm, newdata = Boston[-train, ], n.trees = 1000)
-mean((gbm.yhat - boston.test)^2)
-
-
-# underspecified model
-gbm_under <- gbm(medv ~ ., data = Boston[train_under, ], n.trees = 1000, distribution = "gaussian", interaction.depth = 1)
-gbm_under.yhat <- predict(gbm, newdata = Boston[-train_under, ], n.trees = 1000)
-
-# Bagging (Random Forest with mtry = 12, full variables considered)
-bag <- randomForest(medv ~ ., data = boston_df, mtry = 12, ntree = 1000, subset=train)
-
-#underspecified model
-bag_under <- randomForest(medv ~ ., data = boston_under_df, mtry = 2, ntree = 1000, subset=train)
-
-#calculate the MSE
-
-
-
-
-valuation <- data.frame(
+evaluate_models <- function(ntree) {
+  # Bayesian Additive Regression Trees
+  time <- Sys.time()
+  bart <- gbart(xtrain, ytrain, x.test = xtest, ntree = ntree, mc.cores = 4)
+  bart.mse <- mean((bart$yhat.test.mean - ytest)^2)
+  bart$time <- Sys.time() - time
+  
+  # Random Forest
+  time <- Sys.time()
+  rf <- randomForest(Age ~ ., data = abalone_df, ntree = ntree, subset = train)
+  rf.mse <- mean((predict(rf, newdata = abalone_df[-train, ]) - ytest)^2)
+  rf$time <- Sys.time() - time
+  
+  # Generalized Boosted Regression Models
+  time <- Sys.time()
+  gbm <- gbm(Age ~ ., data = abalone_df[train, ], n.trees = ntree, distribution = "gaussian", interaction.depth = 1)
+  gbm.yhat <- predict(gbm, newdata = abalone_df[-train, ], n.trees = ntree)
+  gbm.mse <- mean((gbm.yhat - ytest)^2)
+  gbm$time <- Sys.time() - time
+  
+  # Bagging (Random Forest with mtry = 8, full variables considered)
+  time <- Sys.time()
+  bag <- randomForest(Age ~ ., data = abalone_df, mtry = 8, ntree = ntree, subset = train)
+  bag.mse <- mean((predict(bag, newdata = abalone_df[-train, ]) - ytest)^2)
+  bag$time <- Sys.time() - time
+  
+  # Return MSEs
+  return(data.frame(
     method = c("BART", "Random Forest", "Boosting", "Bagging"),
-    y_hat = c(mean(bart$yhat.train.mean), mean(rf$predicted), mean(gbm$fit), mean(bag$predicted)),
-    mse = c(bart.mse, mean(rf$mse), mean(gbm$train.error), mean(bag$mse)),
-    mse_under = c(bart_under.mse, mean(rf_under$mse), mean(gbm_under$train.error), mean(bag_under$mse))
-)
+    mse = c(bart.mse, rf.mse, gbm.mse, bag.mse),
+    time = c(bart$time, rf$time, gbm$time, bag$time)
+  ))
+}
 
-print(valuation)
+results <- data.frame()
+
+for (ntree in c(10, 50, 100, 200, 500, 1000, 2000, 5000)) {
+  #suppress all output
+  results_run <- evaluate_models(ntree)
+  results_run$ntree <- ntree
+  results <- rbind(results, results_run)
+  print(paste("ntree:", ntree, "done", " in:", sum(results_run$time)))
+}
+
+write.csv(results, "code/abalone_results.csv")
